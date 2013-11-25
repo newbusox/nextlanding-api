f38358315921a7a009d9390ccfe2d1ef780313ee
Scott Coates <scoarescoare@gmail.com>
2013-11-25 13:19:43 -0500

    disabled debug toolbar

--- scrapy_test/settings/dev.py
+++ scrapy_test/settings/dev.py
@@ -124,7 +124,6 @@ CELERY_RESULT_BACKEND = 'database'
 ########## TOOLBAR CONFIGURATION
 # See: https://github.com/django-debug-toolbar/django-debug-toolbar#installation
 INSTALLED_APPS += (
-    'debug_toolbar',
 )
 
 # See: https://github.com/django-debug-toolbar/django-debug-toolbar#installation
@@ -132,7 +131,6 @@ INTERNAL_IPS = ('127.0.0.1',)
 
 # See: https://github.com/django-debug-toolbar/django-debug-toolbar#installation
 MIDDLEWARE_CLASSES += (
-    'debug_toolbar.middleware.DebugToolbarMiddleware',
 )
 ########## END TOOLBAR CONFIGURATION
 
