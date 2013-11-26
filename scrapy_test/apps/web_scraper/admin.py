b8cc2ffb3942840d4039775263c1df07700b5020
Scott Coates <scoarescoare@gmail.com>
2013-11-25 23:45:33 -0500

    updated streeteasy and admin

--- scrapy_test/apps/web_scraper/admin.py
+++ scrapy_test/apps/web_scraper/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
+from dynamic_scraper.admin import ScraperElemInline
 from scrapy_test.apps.web_scraper.models import ListingSourceScraperConfig, ListingCheckerConfig
 
 admin.site.register(ListingSourceScraperConfig)
 admin.site.register(ListingCheckerConfig)
 
+#ensure the pk field is shown
+ScraperElemInline.readonly_fields = ('pk',)
