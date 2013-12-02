6de40e8c9d355defe28faa937d19b8dcc41070b7
Scott Coates <scoarescoare@gmail.com>
2013-12-02 01:18:27 -0500

    config emailer

--- scrapy_test/apps/domain/constants.py
+++ scrapy_test/apps/domain/constants.py
@@ -4,10 +4,10 @@ POTENTIAL_SEARCH_SESSION_ID = 'potential_search_session_id'
 
 EMAILER_SENDER_SUBJECT_TEMPLATE = "{{ address }} Apartment"
 EMAILER_SENDER_BODY_TEMPLATE = textwrap.dedent("""\
-    Hi{% if apartment.contact_first_name %} {{ apartment.contact_first_name }}{% endif %},
+    Hi{% if contact_name %} {{ contact_name }}{% endif %},
 
-    I saw your listing on {{ source }} for an apartment at {{ apartment.address1 }} ({% if apartment.bedroom %}{{ apartment.bedroom|floatformat }} BR{% else %}studio{% endif %} for ${{ apartment.price|floatformat:"-2" }}). I'm interested in this apartment. Is it still available? If so, when can I see it? Can you tell me anything else about the place?
+    I saw your listing on {{ source }} for an apartment at {{ address }} ({% if bedroom %}{{ bedroom|floatformat }} BR{% else %}studio{% endif %} for ${{ price|floatformat:"-2" }}). I'm interested in this apartment. Is it still available? If so, when can I see it? Can you tell me anything else about the place?
 
     Thanks,
-    {{ signature }}\
+    {{ from_name }}\
 """)
--- scrapy_test/apps/rest_api/views/emailer_sender.py
+++ scrapy_test/apps/rest_api/views/emailer_sender.py
@@ -1,14 +1,8 @@
-from django.http import Http404
-from rest_framework import status
 from rest_framework.response import Response
-from rest_framework.serializers import Serializer
 from rest_framework.views import APIView
 from scrapy_test.aggregates.search.services import search_service
-from scrapy_test.apps.domain.constants import POTENTIAL_SEARCH_SESSION_ID, EMAILER_SENDER_SUBJECT_TEMPLATE, \
+from scrapy_test.apps.domain.constants import EMAILER_SENDER_SUBJECT_TEMPLATE, \
   EMAILER_SENDER_BODY_TEMPLATE
-from scrapy_test.apps.domain.search.models import PotentialSearch
-from scrapy_test.apps.domain.search.services import potential_search_service
-from scrapy_test.apps.rest_api.serializers.potential_search import PotentialSearchSerializer
 
 
 class EmailerSenderView(APIView):
@@ -21,61 +15,25 @@ class EmailerSenderView(APIView):
     search = search_service.get_search(pk)
 
     search_data = {
+      'id': search.pk,
       'location': search.specified_location,
-      'subject_template': EMAILER_SENDER_SUBJECT_TEMPLATE,
-      'body_template': EMAILER_SENDER_BODY_TEMPLATE,
+      'search_description': search.description,
+      'subject': EMAILER_SENDER_SUBJECT_TEMPLATE,
+      'body': EMAILER_SENDER_BODY_TEMPLATE,
     }
 
     return Response(search_data)
 
   def post(self, request, *args, **kwargs):
-    potential_search_id = request.session.get(POTENTIAL_SEARCH_SESSION_ID)
+    pk = kwargs['pk']
+    search = search_service.get_search(pk)
 
-    if not potential_search_id: raise Http404
+    search_data = {
+      'id': search.pk,
+      'location': search.specified_location,
+      'search_description': search.description,
+      'subject': EMAILER_SENDER_SUBJECT_TEMPLATE,
+      'body': EMAILER_SENDER_BODY_TEMPLATE,
+    }
 
-    try:
-      potential_search = potential_search_service.get_potential_search(potential_search_id)
-    except:
-      raise Http404
-
-    data = potential_search_service.get_search_attrs(request.DATA['search_attrs'])
-
-    potential_search.search_attrs = data
-    potential_search_service.save_or_update(potential_search)
-
-    serializer = PotentialSearchSerializer(instance=potential_search)
-
-    return Response(serializer.data)
-
-
-  def create_init(self, request, *args, **kwargs):
-    data = potential_search_service.get_search_attrs(request.DATA['search_attrs'])
-
-    potential_search = PotentialSearch(search_attrs=data)
-
-    potential_search_service.save_or_update(potential_search)
-
-    serializer = PotentialSearchSerializer(instance=potential_search)
-
-    request.session[POTENTIAL_SEARCH_SESSION_ID] = potential_search.id
-
-    return Response(serializer.data, status=status.HTTP_201_CREATED)
-
-
-  def complete_init(self, request, *args, **kwargs):
-    potential_search_id = request.session.get(POTENTIAL_SEARCH_SESSION_ID)
-    token = request.DATA['token']
-
-    if not potential_search_id: raise Http404
-
-    try:
-      potential_search = potential_search_service.get_potential_search(potential_search_id)
-    except:
-      raise Http404
-
-    #this will call save internally
-    potential_search_service.complete_potential_search(potential_search, token)
-
-    serializer = PotentialSearchSerializer(instance=potential_search)
-
-    return Response(serializer.data)
+    return Response(search_data)
