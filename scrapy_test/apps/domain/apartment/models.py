faa066f8ef46d02cb6f8b5290ec0597ec4cb44fd
Scott Coates <scoarescoare@gmail.com>
2013-12-03 21:46:26 -0500

    added apartment

--- scrapy_test/apps/domain/apartment/models.py
+++ scrapy_test/apps/domain/apartment/models.py
@@ -7,6 +7,18 @@ class AddApartmentToSearch(models.Model):
   #do not use foreign keys for aggregates
   apartment_aggregate_id = models.IntegerField(blank=True, null=True)
 
+  lat = models.FloatField()
+  lng = models.FloatField()
+  changed_date = models.DateTimeField(auto_now=True)
+  broker_fee = models.BooleanField()
+  cats_required = models.BooleanField()
+  dogs_required = models.BooleanField()
+  price = models.DecimalField(max_digits=10, decimal_places=2)
+  bedroom_count = models.PositiveSmallIntegerField(max_length=2, blank=True, null=True)
+  bathroom_count = models.DecimalField(max_digits=3, decimal_places=1, blank=True, null=True)
+  sqfeet = models.DecimalField(max_digits=10, decimal_places=3, blank=True, null=True)
+
+
   class Meta:
     app_label = 'domain'
 
--- scrapy_test/apps/domain/apartment/services/add_apartment_to_search_service.py
+++ scrapy_test/apps/domain/apartment/services/add_apartment_to_search_service.py
@@ -9,8 +9,20 @@ def save_or_update(add_apartment_model):
 def get_apartment(pk):
   return SearchEmailerSender.objects.get(pk=pk)
 
+
 def create_apartment(apartment_aggregate):
-  ret_val = AddApartmentToSearch()
+  ret_val = AddApartmentToSearch(
+    lat=apartment_aggregate.lat,
+    lng=apartment_aggregate.lng,
+    changed_date=apartment_aggregate.changed_date,
+    broker_fee=apartment_aggregate.broker_fee,
+    cats_required=bool(apartment_aggregate.amenities.filter(amenity_type__name='Cats').count()),
+    dogs_required=bool(apartment_aggregate.amenities.filter(amenity_type__name='Dogs').count()),
+    price=apartment_aggregate.price,
+    bedroom_count=apartment_aggregate.bedroom_count,
+    bathroom_count=apartment_aggregate.bathroom_count,
+    sqfeet=apartment_aggregate.sqfeet
+  )
 
   save_or_update(ret_val)
 
