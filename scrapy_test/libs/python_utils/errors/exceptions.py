16a313896727be32419f5bcfdb46c870c87c2c81
Scott Coates <scoarescoare@gmail.com>
2013-12-13 07:37:14 -0500

    better printing

--- scrapy_test/libs/python_utils/errors/exceptions.py
+++ scrapy_test/libs/python_utils/errors/exceptions.py
@@ -3,4 +3,8 @@ import sys
 
 
 def re_throw_ex(ex_type, message, inner_ex):
-  raise ex_type, "{0}{sep}Inner Exception: {1}".format(message, inner_ex, sep=os.linesep), sys.exc_info()[2]
+  raise (
+    ex_type,
+    "{0}{sep}Inner Exception: {1}{sep}\t{2}".format(message, type(inner_ex), inner_ex, sep=os.linesep),
+    sys.exc_info()[2] #this is the traceback
+  )
