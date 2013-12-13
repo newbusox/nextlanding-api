2d0843ccbccefb3780e185ba095730d8fca65c67
Scott Coates <scoarescoare@gmail.com>
2013-12-13 06:43:04 -0500

    don't put id in template

--- scrapy_test/apps/domain/constants.py
+++ scrapy_test/apps/domain/constants.py
@@ -9,7 +9,5 @@ EMAILER_SENDER_BODY_TEMPLATE = textwrap.dedent("""\
     I saw your listing on {{ source }} for an apartment at {{ address }} ({% if bedroom %}{{ bedroom|floatformat }} BR{% else %}studio{% endif %} for ${{ price|floatformat:"-2" }}). I'm interested in this apartment. Is it still available? If so, when can I see it? Can you tell me anything else about the place?
 
     Thanks,
-    {{ signature }}
-
-    {{ availability_identifier }}\
+    {{ signature }}\
 """)
