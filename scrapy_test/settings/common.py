12b0b3d64207ab8246687326f6de9ee344bec523
Scott Coates <scoarescoare@gmail.com>
2013-12-13 16:26:59 -0500

    make celery json

--- scrapy_test/settings/common.py
+++ scrapy_test/settings/common.py
@@ -259,6 +259,8 @@ LOGGING = {
 # See: http://celery.readthedocs.org/en/latest/configuration.html#celery-task-result-expires
 CELERY_TASK_RESULT_EXPIRES = timedelta(minutes=30)
 
+CELERY_TASK_SERIALIZER = 'json'
+
 CELERYBEAT_SCHEDULER = 'djcelery.schedulers.DatabaseScheduler'
 
 # See: http://docs.celeryproject.org/en/master/configuration.html#std:setting-CELERY_CHORD_PROPAGATES
