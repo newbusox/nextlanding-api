99375f5c2c6c980430e5468f6e0e78c60ecda4a8
Scott Coates <scoarescoare@gmail.com>
2013-12-13 15:28:58 -0500

    added error output

--- scrapy_test/aggregates/listing/services/listing_tasks.py
+++ scrapy_test/aggregates/listing/services/listing_tasks.py
@@ -1,26 +1,37 @@
+import logging
 from celery.task import task
 from scrapy_test.aggregates.apartment.services import apartment_service
 from scrapy_test.aggregates.listing.services import listing_service
+from scrapy_test.libs.python_utils.errors.exceptions import log_ex_with_message
+
+logger = logging.getLogger(__name__)
 
 
 @task
 def create_listing_task(**listing_attrs):
-  return listing_service.create_listing(**listing_attrs).id
+  try:
+    return listing_service.create_listing(**listing_attrs).id
+  except Exception as e:
+    logger.WARN(log_ex_with_message("Error creating listing", e))
+
 
 @task
 def update_listing_task(**listing_attrs):
   return listing_service.update_listing(**listing_attrs).id
+
 
 @task
 def kill_listing_task(listing_id):
   listing = listing_service.get_listing(listing_id)
   return listing_service.kill_listing(listing).id
 
+
 @task
 def associate_listing_with_apartment_task(listing_id, apartment_id):
   listing = listing_service.get_listing(listing_id)
   apartment = apartment_service.get_apartment(apartment_id)
   return listing_service.associate_listing_with_apartment(listing, apartment).id
+
 
 @task
 def notify_listings_unavailable_task(apartment_id):
--- scrapy_test/libs/python_utils/errors/exceptions.py
+++ scrapy_test/libs/python_utils/errors/exceptions.py
@@ -5,6 +5,10 @@ import sys
 def re_throw_ex(ex_type, message, inner_ex):
   raise (
     ex_type,
-    "{0}{sep}Inner Exception: {1}{sep}\t{2}".format(message, type(inner_ex), inner_ex, sep=os.linesep),
+    log_ex_with_message(message, inner_ex),
     sys.exc_info()[2] #this is the traceback
   )
+
+
+def log_ex_with_message(message, inner_ex):
+  return "{0}{sep}Inner Exception: {1}{sep}\t{2}".format(message, type(inner_ex), inner_ex, sep=os.linesep)
