fa67843145279bf1296844139150b4fda1dd1478
Scott Coates <scoarescoare@gmail.com>
2013-12-15 14:32:28 -0500

    fix ex

--- nextlanding_api/libs/python_utils/errors/exceptions.py
+++ nextlanding_api/libs/python_utils/errors/exceptions.py
@@ -11,4 +11,4 @@ def re_throw_ex(ex_type, message, inner_ex):
 
 
 def log_ex_with_message(message, inner_ex):
-  return "{0}{sep}Inner Exception: {1}{sep}\t{2}".format(message, type(inner_ex), inner_ex, sep=os.linesep)
+  return u"{0}{sep}Inner Exception: {1}{sep}\t{2}".format(message, type(inner_ex), inner_ex, sep=os.linesep)
