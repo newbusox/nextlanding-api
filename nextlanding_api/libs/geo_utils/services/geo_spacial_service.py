80725b5da56c268c5caa2edbe9d0cf36acf924e1
Scott Coates <scoarescoare@gmail.com>
2013-12-18 21:17:42 -0500

    simplified

--- nextlanding_api/libs/geo_utils/services/geo_spacial_service.py
+++ nextlanding_api/libs/geo_utils/services/geo_spacial_service.py
@@ -1,13 +1,7 @@
 from django.contrib.gis.geos import Point, Polygon, MultiPolygon
 
 
-def point_resides_in_bounds(point, polygon):
-  poly = Polygon(polygon)
-  prep_poly = poly.prepared
-  return prep_poly.contains(Point(point))
-
-
-def point_resides_in_multi_bounds(point, *polygons):
+def point_resides_in_bounds(point, *polygons):
   polygons = [Polygon(polygon) for polygon in polygons]
   multi = MultiPolygon(polygons)
   prep_multi = multi.prepared
--- nextlanding_api/libs/geo_utils/tests/unit/test_geo_spatial_service.py
+++ nextlanding_api/libs/geo_utils/tests/unit/test_geo_spatial_service.py
@@ -1,7 +1,6 @@
 import pytest
 from nextlanding_api.libs.geo_utils.services import geo_spacial_service
 
-
 @pytest.mark.parametrize(("polygon", "point"), [
   (
     (
@@ -25,7 +24,6 @@ from nextlanding_api.libs.geo_utils.services import geo_spacial_service
 def test_point_resides_in_bounds(polygon, point):
   assert geo_spacial_service.point_resides_in_bounds(point, polygon)
 
-
 @pytest.mark.parametrize(("polygons", "point"), [
   (
     (
@@ -44,6 +42,23 @@ def test_point_resides_in_bounds(polygon, point):
     ),
     (40.772233, -73.979583)
   ),
+  (
+    (
+      (
+        (40.7560997756636, -73.99411618709564),
+        (40.762341053140275, -73.95566403865814),
+        (40.74595644996025, -73.97729337215424),
+        (40.7560997756636, -73.99411618709564),
+      ),
+      (
+        (40.769101775774935, -73.99068295955658),
+        (40.76858174460538, -73.94364774227142),
+        (40.79353864997009, -73.97660672664642),
+        (40.769101775774935, -73.99068295955658),
+      )
+    ),
+    (40.7546308, -73.972055)
+  ),
 ])
 def test_point_resides_in_multi_bounds(polygons, point):
-  assert geo_spacial_service.point_resides_in_multi_bounds(point, *polygons)
+  assert geo_spacial_service.point_resides_in_bounds(point, *polygons)
