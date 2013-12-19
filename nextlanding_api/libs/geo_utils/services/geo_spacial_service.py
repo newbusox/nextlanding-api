52232ab91d76811632d0470efd372ce5b148ddbb
Scott Coates <scoarescoare@gmail.com>
2013-12-18 21:06:16 -0500

    added multi bounds

--- nextlanding_api/libs/geo_utils/services/geo_spacial_service.py
+++ nextlanding_api/libs/geo_utils/services/geo_spacial_service.py
@@ -1,7 +1,15 @@
-from django.contrib.gis.geos import Point, Polygon
+from django.contrib.gis.geos import Point, Polygon, MultiPolygon
 
 
-def point_resides_in_bounds(polygon, point):
+def point_resides_in_bounds(point, polygon):
   poly = Polygon(polygon)
   prep_poly = poly.prepared
   return prep_poly.contains(Point(point))
+
+
+def point_resides_in_multi_bounds(point, *polygons):
+  polygons = [Polygon(polygon) for polygon in polygons]
+  multi = MultiPolygon(polygons)
+  prep_multi = multi.prepared
+
+  return prep_multi.contains(Point(point))
