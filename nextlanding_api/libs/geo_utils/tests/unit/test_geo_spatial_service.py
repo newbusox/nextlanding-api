b8e0a52df740f93f087dadecc94606b972325b63
Scott Coates <scoarescoare@gmail.com>
2013-12-18 20:40:48 -0500

    more test

--- nextlanding_api/libs/geo_utils/tests/unit/test_geo_spatial_service.py
+++ nextlanding_api/libs/geo_utils/tests/unit/test_geo_spatial_service.py
@@ -13,6 +13,15 @@ from nextlanding_api.libs.geo_utils.services.geo_spacial_service import point_re
     ),
     (40.7546308, -73.972055)
   ),
+  (
+    (
+      (40.769101775774935,-73.99068295955658),
+      (40.76858174460538,-73.94364774227142),
+      (40.79353864997009,-73.97660672664642),
+      (40.769101775774935,-73.99068295955658),
+    ),
+    (40.772233, -73.979583)
+  ),
 ])
 def test_point_resides_in_bounds(polygon, point):
   assert point_resides_in_bounds(polygon, point)
