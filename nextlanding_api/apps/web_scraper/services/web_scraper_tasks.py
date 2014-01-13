721a8118d217b87062bafb9ddd093fde5797ab02
Scott Coates <scoarescoare@gmail.com>
2014-01-13 15:10:45 -0500

    log error deleting checker

--- nextlanding_api/apps/web_scraper/services/web_scraper_tasks.py
+++ nextlanding_api/apps/web_scraper/services/web_scraper_tasks.py
@@ -1,6 +1,11 @@
+import logging
 from celery import shared_task
+from celery.exceptions import Ignore
 from nextlanding_api.aggregates.listing.services import listing_service
 from nextlanding_api.apps.web_scraper.services import web_scraper_service
+from nextlanding_api.libs.python_utils.errors.exceptions import log_ex_with_message
+
+logger = logging.getLogger(__name__)
 
 
 @shared_task()
@@ -22,8 +27,13 @@ def add_listing_checker_task(listing_id):
 
 @shared_task()
 def delete_listing_checker_task(listing_id):
-  listing = listing_service.get_listing(listing_id)
-  web_scraper_service.delete_listing_checker(listing)
+  try:
+    listing = listing_service.get_listing(listing_id)
+    web_scraper_service.delete_listing_checker(listing)
+  except Exception as e:
+    logger.info(log_ex_with_message(u"Error deleting checker", e))
+    raise Ignore()
+
 
 @shared_task()
 def crawl_individual_page_task(crawl_url):
