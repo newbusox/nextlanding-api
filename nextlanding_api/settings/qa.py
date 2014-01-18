c554098555e949ebf4c3bbfde8856304821f0dff
Scott Coates <scoarescoare@gmail.com>
2014-01-18 16:55:55 -0500

    only one worker

--- nextlanding_api/settings/qa.py
+++ nextlanding_api/settings/qa.py
@@ -114,7 +114,7 @@ BROKER_POOL_LIMIT = None
 
 # Do not scale up to 4 processes (default in heroku) but limit it to two. This will end up using less connections to
 # redis
-CELERYD_CONCURRENCY = 2
+CELERYD_CONCURRENCY = 1
 ########## END CELERY CONFIGURATION
 
 ########## STORAGE CONFIGURATION
